4221de16b817198b4e68593b3ecd47eab3327af7
Airton659 <108145077+Airton659@users.noreply.github.com>
2025-07-20 11:44:25 -0300

    Add network discovery

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import 'models/device.dart';
+import 'services/discovery_service.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -7,116 +10,186 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'TV Remote',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const DeviceDiscoveryPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class DeviceDiscoveryPage extends StatefulWidget {
+  const DeviceDiscoveryPage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<DeviceDiscoveryPage> createState() => _DeviceDiscoveryPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _DeviceDiscoveryPageState extends State<DeviceDiscoveryPage> {
+  final List<Device> _devices = [];
+  bool _scanning = false;
 
-  void _incrementCounter() {
+  Future<void> _scan() async {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _scanning = true;
+      _devices.clear();
     });
+
+    final discovery = DiscoveryService();
+    final results = await discovery.discoverDevices();
+
+    setState(() {
+      _devices.addAll(results);
+      _scanning = false;
+    });
+  }
+
+  void _openRemote(Device device) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => RemoteControlPage(device: device),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(title: const Text('TV Discovery')),
+      body: Column(
+        children: [
+          if (_scanning) const LinearProgressIndicator(),
+          Expanded(
+            child: _devices.isEmpty
+                ? Center(
+                    child: _scanning
+                        ? const Text('Scanning for devices...')
+                        : const Text('No devices found.'),
+                  )
+                : ListView.builder(
+                    itemCount: _devices.length,
+                    itemBuilder: (context, index) {
+                      final device = _devices[index];
+                      return ListTile(
+                        title: Text(device.name),
+                        subtitle: Text(device.address),
+                        trailing: const Icon(Icons.chevron_right),
+                        onTap: () => _openRemote(device),
+                      );
+                    },
+                  ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: _scanning ? null : _scan,
+        icon: const Icon(Icons.search),
+        label: const Text('Scan for TVs'),
+      ),
+    );
+  }
+}
+
+class RemoteControlPage extends StatelessWidget {
+  final Device device;
+  const RemoteControlPage({super.key, required this.device});
+
+  void _sendCommand(String command) {
+    debugPrint('Send command $command to ${device.address}');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final buttons = [
+      _RemoteControlButton(
+        icon: Icons.volume_up,
+        label: 'Vol +',
+        onPressed: () => _sendCommand('volume_up'),
+      ),
+      _RemoteControlButton(
+        icon: Icons.volume_down,
+        label: 'Vol -',
+        onPressed: () => _sendCommand('volume_down'),
+      ),
+      _RemoteControlButton(
+        icon: Icons.arrow_drop_up,
+        label: 'Chan +',
+        onPressed: () => _sendCommand('channel_up'),
+      ),
+      _RemoteControlButton(
+        icon: Icons.arrow_drop_down,
+        label: 'Chan -',
+        onPressed: () => _sendCommand('channel_down'),
+      ),
+      _RemoteControlButton(
+        icon: Icons.apps,
+        label: 'Apps',
+        onPressed: () => _sendCommand('apps'),
+      ),
+      _RemoteControlButton(
+        icon: Icons.menu,
+        label: 'Menu',
+        onPressed: () => _sendCommand('menu'),
+      ),
+      _RemoteControlButton(
+        icon: Icons.reply,
+        label: 'Last',
+        onPressed: () => _sendCommand('last_channel'),
+      ),
+      _RemoteControlButton(
+        icon: Icons.power_settings_new,
+        label: 'Power',
+        onPressed: () => _sendCommand('power'),
+      ),
+    ];
+
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text(device.name),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: GridView.count(
+          crossAxisCount: 2,
+          crossAxisSpacing: 16,
+          mainAxisSpacing: 16,
+          children: buttons,
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class _RemoteControlButton extends StatelessWidget {
+  final IconData icon;
+  final String label;
+  final VoidCallback onPressed;
+
+  const _RemoteControlButton({
+    required this.icon,
+    required this.label,
+    required this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(padding: const EdgeInsets.all(16)),
+      onPressed: onPressed,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(icon, size: 32),
+          const SizedBox(height: 8),
+          Text(label),
+        ],
+      ),
     );
   }
 }
---
+++ lib/models/device.dart
@@ -0,0 +1,5 @@
+class Device {
+  final String name;
+  final String address;
+  const Device({required this.name, required this.address});
+}
